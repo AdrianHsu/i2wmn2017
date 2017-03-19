2a776a92fe32c12c5dc7710a32cd5e1da577d1dc
AdrianHsu <adrianhsu1995@gmail.com>
2017-03-19 10:51:21 +0800

    done

--- hw1/tmp2.m
+++
@@ -1,2 +0,0 @@
-d = [-3:.1:3]; %d: reference distance, the distance between the BS and the mobile device (in meter) as the x-axis.
-norm = normpdf(d,0,1);
--- hw1/todB.m
+++
@@ -1,3 +0,0 @@
-function x = todB(G_d)
-x = 10*log10(G_d)
-end
